23ebcdafb46fd4eab7e8154f1248b9e2257311cd
The Night <36819550+lasser02@users.noreply.github.com>
2018-11-21 21:35:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,16 @@ bot.on('ready', () => {
     bot.user.setActivity("with servers | c!help")
 });
 
+bot.on('message', msg  => {
+    if (msg.author.bot || !msg.content.startsWith("c!")) return;
+    const args  = msg.content.slice("c!".length).split(" ");
+    const command = args.shift().toLowerCase();
+   
+    if (command == "hru") {
+    if (!args[0]) return msg.channel.send("i'm good thx for asking")
+    const saytxt = args.join(" ");
+    msg.channel.send(saytxt)
+    };
 
 bot.on('message', msg  => {
     if (msg.author.bot || !msg.content.startsWith("c!")) return;
