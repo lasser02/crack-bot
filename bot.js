0d76dd6641a8387490fa525851f08e6a26ea77ff
The Night <36819550+lasser02@users.noreply.github.com>
2018-11-21 21:12:49 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+bot.on('ready', () => {
+    console.log("Bin Ready!");
+    bot.user.setActivity("with servers | c!help")
+});
+
+
+bot.on('message', msg  => {
+    if (msg.author.bot || !msg.content.startsWith("c!")) return;
+    const args  = msg.content.slice("c!".length).split(" ");
+    const command = args.shift().toLowerCase();
+   
+    if (command == "say") {
+    if (!args[0]) return msg.channel.send("syntax: c!say <Text>")
+    const saytxt = args.join(" ");
+    msg.channel.send(saytxt)
+    };
+});
+
+bot.login(process.env.BOT_TOKEN)
